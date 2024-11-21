1cd64e85ae4bfaccb8b3a86ae9ebfca2e5aff62c
Fariza Zea De Asminto <farizazea9c@gmail.com>
2024-11-21 15:32:02 +0700

    Add files via upload

---
+++ DDP_9.py
@@ -0,0 +1,26 @@
+print("--------celcius_ke_fahrenheit-------")
+def celcius_ke_fahrenheit(celcius):
+   print(celcius * 9/5 +32)
+celcius_ke_fahrenheit(0)
+
+print("\n--------Mencari bilangan genap")
+def is_genap(genap):
+  print(genap %2 == 0)
+is_genap(4)
+is_genap(7)
+
+print("\n--------Keterangan lulus dan tidak lulus--------")
+#rata" nilai kelulusan 70
+def skor(nilai):
+  if nilai >= 80:
+      print("Lulus")
+  else:
+    print("Gagal")
+skor(80)
+skor(60)
+
+print("\n--------Mencari bilangan ganjil--------")
+def bil_ganjil(ganjil):
+  for i in range(1,ganjil+1, 2):
+    print(i, end= "")
+bil_ganjil(20)
